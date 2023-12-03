271f9cda057a804c4a11eafc1b0787e0714a40fd
divyesh0406 <divyeshrmistry@gmail.com>
2023-12-03 16:27:09 +0530

    Finish Project

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 from langchain.memory import CassandraChatMessageHistory,ConversationBufferMemory
-from langchain.llms import openai
+from langchain.llms import OpenAI
 from langchain import LLMChain , PromptTemplate
 import json
 
@@ -15,7 +15,7 @@ with open("choose_your_adventure-token.json") as f:
 CLIENT_ID = secrets["clientId"]
 CLIENT_SECRET = secrets["secret"]
 ASTRA_DB_KEYSPACE ="database"
-OPENAI_API_KEY =""
+OPENAI_API_KEY ="{EnterYourOwnOpenAIapiKey}"
 
 auth_provider = PlainTextAuthProvider(CLIENT_ID, CLIENT_SECRET)
 cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
@@ -57,9 +57,21 @@ prompt = PromptTemplate(
     template = template
 )
 
-llm = openai(OPENAI_API_KEY=OPENAI_API_KEY)
+llm = OpenAI(openai_api_key = OPENAI_API_KEY)
 llm_chain = LLMChain(
     llm = llm,
     prompt = prompt,
     memory = cass_buff_memory
 )
+
+choice = "start"
+
+while True:
+    response = llm_chain.predict(human_input=choice)
+    print(response.strip())
+    if "The End." in response: 
+        break
+    choice = input("Your reply: ")
+
+
+
